2ba18400f51a65dcaa536dc9397e0717ab31359a
Eugene Lee <eug703@gmail.com>
2019-10-08 01:16:40 -0400

    html and css complete. JS work being done.

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function({
+    
+    
+    <script>
+  // Your web app's Firebase configuration
+      var firebaseConfig = {
+        apiKey: "AIzaSyBIf1fMiQYAJZYutLQN87sSALlDK2K2oSI",
+        authDomain: "runningtrain69-9a6fd.firebaseapp.com",
+        databaseURL: "https://runningtrain69-9a6fd.firebaseio.com",
+        projectId: "runningtrain69-9a6fd",
+        storageBucket: "",
+        messagingSenderId: "74674344288",
+        appId: "1:74674344288:web:c7b008953b2e3ef94ace05"
+      };
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    </script>
+
+    var database = firebase.database()
+
+
+
+
+
+}
+
+
+
+
+
+
+}
